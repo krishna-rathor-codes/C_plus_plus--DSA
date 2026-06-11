67758800c75634d4b2e4bfab4d690769effb0d34
krishna-rathor-codes <this.is.krishna.cs@gmail.com>
2026-06-11 18:47:23 +0530

    rarrange all questions

--- new_project.cpp
+++ new_project.cpp
@@ -1,86 +1,120 @@
-#include<iostream>
-using namespace std;
-
-int prime(int p){
-    if(p%2 != 0){
-        cout<<"no. is prime";
-    }
-    else
-        cout<<"no. is not prime";
-
-        return 0;
-}
-
-
-    int factriolN(int n) {
-    int fac = 1;
-    for(int i=1; i<=n; i++){
-        fac *= i;
-
-    }
-    return fac;
-
-}
+                                                                // Functions
 
 
 
 
-int decTObinary(int decNum) {
-    int ans = 0;
-    int power = 1;
+// #include<iostream>
+// using namespace std;
 
-    while(decNum > 0 ) {
-        int rem = decNum%2;
-        decNum = decNum/2;
-        ans += (rem*power);
-        power *= 10;
-    }
-    return ans;
-}
+// int prime(int p){
+//     if(p%2 != 0){
+//         cout<<"no. is prime";
+//     }
+//     else
+//         cout<<"no. is not prime";
+
+//         return 0;
+// }
+
+
+//     int factriolN(int n) {
+//     int fac = 1;
+//     for(int i=1; i<=n; i++){
+//         fac *= i;
+
+//     }
+//     return fac;
+
+// }
+
+
+
+
+// int decTObinary(int decNum) {
+//     int ans = 0;
+//     int power = 1;
+
+//     while(decNum > 0 ) {
+//         int rem = decNum%2;
+//         decNum = decNum/2;
+//         ans += (rem*power);
+//         power *= 10;
+//     }
+//     return ans;
+// }
 
 
 
 
   
+// int main(){
+
+
+// cout<<decTObinary(23);
+
+// return 0;
+
+// }
+
+
+
+
+
+#include<iostream>
+using namespace std;
 int main(){
 
-  // int n;
-    // cout<<"Enter the a numbers : ";
-    // cin>>n;
-    // int sum = 0;
-    // for(int i = 0;i<=n;i++){
-    //     if(i%3==0){
-    //         sum += i;
+
+                                           
+
+                                                    //prime number/sum of n natural number/factioral/character pattern
+
+
+
+
+//   int n;
+//     cout<<"Enter the a numbers : ";
+//     cin>>n;
+//     int sum = 0;
+//     for(int i = 0;i<=n;i++){
+//         if(i%3==0){
+//             sum += i;
             
-    //     }
+//         }
         
-    // }
-    // cout<<sum;
+//     }
+//     cout<<sum;
 
-    // int n;
-    // cout<<"enter the number : ";
-    // cin>>n;
-    // int mul = 1;
-    // for(int i = 1;i<=n;i++){
-    //     mul *= i;
+//     int n;
+//     cout<<"enter the number : ";
+//     cin>>n;
+//     int mul = 1;
+//     for(int i = 1;i<=n;i++){
+//         mul *= i;
 
-    // }
-    // cout<<mul;
+//     }
+//     cout<<mul;
 
-    // int n;
-    // cout<<"enter the number of star : ";
-    // cin>>n;
-    // char ch = 'A';
+//     int n;
+//     cout<<"enter the number of star : ";
+//     cin>>n;
+//     char ch = 'A';
     
 
-    // for(int i = 0; i<n; i++){
-    //     for(int j = 0; j<i+1; j++){
-    //         cout<<ch;
-    //     }
-    //     ch =ch +1;
-    //     cout<<endl;
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<i+1; j++){
+//             cout<<ch;
+//         }
+//         ch =ch +1;
+//         cout<<endl;
     
 // }
+
+
+
+
+                                                        //butterfly pattern
+                                                    
 
 // int n;
 // cout<<"enter the no. : ";
@@ -119,7 +153,6 @@ int main(){
 //     cout<<endl;
 // }
 
-cout<<decTObinary(23);
 
 
 
@@ -128,31 +161,5 @@ cout<<decTObinary(23);
 
 
 
-
-
-
-
-
-
-
-
-
-
-    
-
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-return 0;
 
 }
